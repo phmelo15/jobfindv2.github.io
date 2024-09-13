e99eaa5426c2afd6ed499cada4ddffcb577c669d
Pedro <phmelo15@gmail.com>
2024-09-13 15:07:19 -0300

    fix

--- src/app/FindJob/page.tsx
+++ src/app/FindJob/page.tsx
@@ -1,5 +1,4 @@
 import HeaderPrivate from "@/components/HeaderPrivate/HeaderPrivate";
-import SignatureCard from "@/components/SignatureCard/SignatureCard";
 import React from "react";
 import { styles } from "./styles";
 import JobsFilter from "@/components/JobsFilter/JobsFilter";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Login from "./Login/page";
 import Home from "./Home/page";
 
 export default function page() {
